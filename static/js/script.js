e8cba845c90cada18e0c6452f5d38d8d6b6c1c3c
Sanaz Katpitia <sanazkatpitia@gmail.com>
2026-03-08 13:35:11 +0530

    Remove auction bid fetch and log script load

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,2 @@
-setInterval(function(){
 
-let playerId = window.location.pathname.split("/")[2]
-
-fetch("/highest/"+playerId)
-
-.then(res=>res.json())
-
-.then(data=>{
-
-document.getElementById("highestBid").innerText = data.bid
-
-})
-
-},2000)
+console.log("Auction scripts loaded")
